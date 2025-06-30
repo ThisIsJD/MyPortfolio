983bcd52def511aeedaa2fc8b95f046f6f1e5958
ThisIsJD <johndaniel.labing@unc.edu.ph>
2025-06-30 20:45:05 +0800

    Certification section and Tech Stack section is done, Both is not perfect yet but it's already good enough.

--- indexjs.js
+++ indexjs.js
@@ -42,8 +42,6 @@ window.addEventListener('scroll', () => {
 //     }
 //     })
 
-
-
 //     let prevScrollpos = window.pageYOffset;
 // window.onscroll = function() {
 //   let currentScrollPos = window.pageYOffset;
@@ -54,3 +52,56 @@ window.addEventListener('scroll', () => {
 //   }
 //   prevScrollpos = currentScrollPos;
 // }
+
+ // 1) “Install” the modules you plan to use
+  Swiper.use([Swiper.EffectCoverflow, Swiper.Pagination, Swiper.Navigation]);
+
+  // 2) Initialize Swiper on your .swiper container
+new Swiper('.swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 60,
+
+     autoplay: {
+      delay: 3000,           // 3 seconds
+      disableOnInteraction: false,  
+      // (keeps autoplay running even after you manually swipe)
+    },
+
+    // Coverflow settings
+    coverflowEffect: {
+      rotate: 35,     // how much the side slides rotate (degrees)
+      stretch: 0,     // space between slides in px
+      depth: 200,     // depth offset in px (perspective)
+      modifier: 1,    // multiplier for all params
+      slideShadows: true,
+    },
+
+    // Pagination (dots)
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    // Navigation arrows (if you have them)
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+     breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+  });
